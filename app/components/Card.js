87957efe282462bbd51cc77daa82747e044ec773
Alexis <a.goutorbe@gmail.com>
2021-01-29 11:57:02 +0900

    authentification

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { View, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native'
+import React from "react";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { Image } from "react-native-expo-image-cache";
 
-import Text from './Text'
-import colors from '../config/colors'
+import Text from "./Text";
+import colors from "../config/colors";
 
-function Card({ title, subTitle, imageUrl, onPress }) {
+function Card({ title, subTitle, imageUrl, onPress, thumbnailUrl }) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={{ uri: imageUrl }} />
+        <Image
+          style={styles.image}
+          tint="light"
+          preview={{ uri: thumbnailUrl }}
+          uri={imageUrl}
+        />
         <View style={styles.detailsContainer}>
           <Text style={styles.title} numberOfLines={1}>
             {title}
@@ -19,7 +25,7 @@ function Card({ title, subTitle, imageUrl, onPress }) {
         </View>
       </View>
     </TouchableWithoutFeedback>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -27,22 +33,22 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: colors.white,
     marginBottom: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   detailsContainer: {
     padding: 20,
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 200,
   },
   subTitle: {
     color: colors.secondary,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   title: {
     marginBottom: 7,
   },
-})
+});
 
-export default Card
+export default Card;
